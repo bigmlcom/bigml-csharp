b3b7ad38c62cadfa8f7fbfebd341807d2dfcb3a4
Jose Ribes <joseribesmartinez@gmail.com>
2013-12-27 14:56:22 +0100

    method that use the model in each node

--- BigML/Model/Description.cs
+++ BigML/Model/Description.cs
@@ -57,6 +57,26 @@ namespace BigML
                 return lambda;
             }
 
+
+            /// <summary>
+            /// Compile Model description to Expression using ResultNode objects
+            /// </summary>
+            public LambdaExpression ComplexResult()
+            {
+                var parameters = new Dictionary<string, ParameterExpression>();
+                foreach (var field in Fields)
+                {
+                    if (!_inputFields.Contains(field.Key)) continue;
+                    var type = field.Value.OpType.TypeOf();
+                    var parameter = System.Linq.Expressions.Expression.Parameter(type, field.Value.Name);
+                    parameters[field.Key] = parameter;
+                }
+
+                var body = Root.ComplexResult(parameters);
+                var lambda = System.Linq.Expressions.Expression.Lambda(body, parameters.Values);
+                return lambda;
+            }
+
             /// <summary>
             /// A dictionary with an entry per field in the model. 
             /// Each entry includes the column number in original source, the name of the field, the type of the field, and the summary.
