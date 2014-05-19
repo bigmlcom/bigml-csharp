a2d97ed96ebea00e069afcc69a249d5c77fb1e93
Jose Ribes <joseribesmartinez@gmail.com>
2014-05-19 17:25:28 +0200

    Added the clusters status property

---
+++ BigML/Cluster/Status.cs
@@ -0,0 +1,28 @@
+using System.Json;
+
+namespace BigML
+{
+    public partial class Cluster
+    {
+        /// <summary>
+        /// Creating a cluster is a process that can take just a few seconds or a few days depending on the size of the dataset used as input and on the work load of BigML's systems. 
+        /// The cluster goes through a number of states until its fully completed.
+        /// Through the status field in the cluster you can determine when the cluster has been fully processed and ready to be used to create predictions. 
+        /// </summary>
+        public class Status : Status<Cluster>
+        {
+            internal Status(JsonValue status): base(status)
+            {
+            }
+
+            /// <summary>
+            /// How far BigML.io has progressed processing the cluster.
+            /// </summary>
+            public double Progress
+            {
+                get { return _status.progress; }
+            }
+
+        }
+    }
+}
