622c0bfc74eac60f9a42c04bc2866a5315c35a78
Jose Ribes <joseribesmartinez@gmail.com>
2014-05-22 19:25:39 +0200

    updated the clusters arguments
    only dataset and k

--- BigML/Cluster/BigMLClientCluster.cs
+++ BigML/Cluster/BigMLClientCluster.cs
@@ -19,16 +19,15 @@ namespace BigML
         /// <param name="name">The name you want to give to the new cluster. </param>
         /// <param name="inputFields">Specifies the ids of the fields that you want to use as important to create the cluster. </param>
         /// <param name="objectiveField">Specifies the id of the field that you want to predict.</param>
-        public Task<Cluster> CreateCluster(DataSet dataset, string name = null, string[] inputFields = null,
-                                    string objectiveField = null, Cluster.Arguments arguments = null)
+        public Task<Cluster> CreateCluster(DataSet dataset, string name = null, int k = 8,
+                                            Cluster.Arguments arguments = null)
         {
             arguments = arguments ?? new Cluster.Arguments();
             if (!string.IsNullOrWhiteSpace(name)) arguments.Name = name;
-            if (inputFields != null && inputFields.Length > 0)
+            if (k != null && k > 0)
             {
-                foreach(var input in arguments.Inputs) arguments.Inputs.Add(input);
+                arguments.NumberOfCluster = k;
             }
-            if (!string.IsNullOrWhiteSpace(objectiveField)) arguments.Objective = objectiveField;
             arguments.DataSet = dataset.Resource;
             return Create<Cluster>(arguments);
         }
