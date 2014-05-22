efd60aa9ede3b30b03a25114c0a2fcfa655f79fa
Jose Ribes <joseribesmartinez@gmail.com>
2014-05-22 18:32:35 +0200

    new properties for clusters: name and status

--- BigML/Cluster/Cluster.cs
+++ BigML/Cluster/Cluster.cs
@@ -9,12 +9,41 @@ namespace BigML
     public partial class Cluster : Response
     {
 
+
+        /// <summary>
+        /// The name of the cluster as your provided or based on the name of the dataset by default.
+        /// </summary>
+        public string Name
+        {
+            get { return Object.name; }
+        }
+
+
         /// <summary>
         /// The dataset/id that was used to build the dataset.
         /// </summary>
         public string DataSet
         {
             get { return Object.dataset; }
+        }
+
+
+
+        /// <summary>
+        /// Whether the dataset is still available or has been deleted.
+        /// </summary>
+        public bool DataSetStatus
+        {
+            get { return Object.dataset_status; }
+        }
+
+
+        /// <summary>
+        /// A description of the status of the cluster. It includes a code, a message, and some extra information.
+        /// </summary>
+        public Status StatusMessage
+        {
+            get { return new Status(Object.status); }
         }
     }
 }
