48cc873a1bfbb3d0ef6ee2c4a89a4904c5c531cd
Jose Ribes <joseribesmartinez@gmail.com>
2015-03-11 17:26:29 +0100

    give support for batch predictions using the bigML API
    https://bigml.com/developers/batch_predictions

---
+++ BigML/BatchPrediction/Status.cs
@@ -0,0 +1,32 @@
+using System.Json;
+
+namespace BigML
+{
+    public partial class BatchPrediction
+    {
+        /// <summary>
+        /// Creating an anomaly detector is a process that can take just a few
+        /// seconds or a few days depending on the size of the dataset used as
+        /// input and on the work load of BigML's systems.
+        /// The batch prediction goes through a number of states until its
+        /// fully completed.
+        /// Through the status field in the batchprediction you can determine
+        /// when the batch prediction has been fully completed.
+        /// </summary>
+        public class Status : Status<BatchPrediction>
+        {
+            internal Status(JsonValue status): base(status)
+            {
+            }
+
+            /// <summary>
+            /// How far BigML.io has progressed processing the Batch Prediction.
+            /// </summary>
+            public double Progress
+            {
+                get { return _status.progress; }
+            }
+
+        }
+    }
+}
