26f4723ee5de93e33994eefe7d6a5736dec96acc
Jose Ribes <joseribesmartinez@gmail.com>
2015-07-02 16:00:30 +0200

    Add three initial Gists

---
+++ Gists/ModelFromAzureSource.cs
@@ -0,0 +1,36 @@
+﻿using BigML;
+using System;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class ModelFromAzureSource
+    {
+        static async void Main()
+        {
+            // New BigML client with username and API key
+            Console.Write("user: "); var User = Console.ReadLine();
+            Console.Write("key: "); var ApiKey = Console.ReadLine();
+            var client = new Client(User, ApiKey);
+
+            // Create a source from a file in azure storage
+            var remoteURL = @"azure://csv/iris.csv?AccountName=bigmlpublic";
+            Source.Arguments remoteArguments = new Source.Arguments();
+            remoteArguments.Remote = remoteURL;
+            remoteArguments.Name = "Iris from Azure";
+            var sourceFromAzure = await client.Create(remoteArguments);
+            while ((sourceFromAzure = await client.Get(sourceFromAzure)).StatusMessage.NotSuccessOrFail()) await Task.Delay(10);
+
+            // Create a dataset from this sources
+            var dataset = await client.Create(sourceFromAzure);
+            while ((dataset = await client.Get(dataset)).StatusMessage.NotSuccessOrFail()) await Task.Delay(10);
+            Console.WriteLine(dataset.StatusMessage.ToString());
+
+            // Default model from this dataset
+            var model = await client.Create(dataset);
+            // No push, so we need to busy wait for the source to be processed.
+            while ((model = await client.Get(model)).StatusMessage.NotSuccessOrFail()) await Task.Delay(10);
+            Console.WriteLine(model.StatusMessage.ToString());
+        }
+    }
+}
---
+++ Gists/PredictionWithIrisEnsemble.cs
@@ -0,0 +1,27 @@
+﻿using BigML;
+using System;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class PredictionWithIrisEnsemble
+    {
+        static async void Main()
+        {
+            // New BigML client in production mode with username and API key
+            Console.Write("user: "); var User = Console.ReadLine();
+            Console.Write("key: "); var ApiKey = Console.ReadLine();
+            var client = new Client(User, ApiKey);
+
+            var parameters = new Prediction.Arguments();
+            parameters.Add("ensemble", "ensemble/54ad6d0558a27e2ddf000XXX"); //put your Id here
+            parameters.InputData.Add("000000", 7.9);
+            parameters.InputData.Add("000001", 3.8);
+            parameters.InputData.Add("000002", 6.4);
+            parameters.InputData.Add("000003", 2);
+            Prediction ensemblePrediction;
+            while ((ensemblePrediction = await client.CreatePrediction(parameters)).StatusMessage.NotSuccessOrFail()) await Task.Delay(5);
+            Console.WriteLine(ensemblePrediction.GetPredictionOutcome<string>().ToString());  //output is string for categorical models
+        }
+    }
+}
