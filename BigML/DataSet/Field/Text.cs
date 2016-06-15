2eaa7d6bc9424635c1d6022aeacf67d5981ea20d
Jose <ribes@bigml.com>
2016-06-15 19:51:36 +0200

    Add  summary for fields of type Text

--- BigML/DataSet/Field/Text.cs
+++ BigML/DataSet/Field/Text.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Collections;
 using System.Json;
 
 namespace BigML
@@ -19,16 +20,49 @@ namespace BigML
                     {
                     }
 
-                    /// <summary>
-                    /// A dictionary where the keys are the unique categories found in the field and the values are the count for that category.
-                    /// </summary>
-                    public IDictionary<string, int> Counts
+                    public double AverageLength
                     {
                         get
                         {
-                            var counts = new Dictionary<string, int>();
-                            foreach (var kv in _summary.counts) counts[kv.Key] = kv.Value;
-                            return counts;
+                            return _summary.average_length;
+                        }
+                    }
+
+                    /// <summary>
+                    /// A dictionary where the keys are the unique categories found in the field and the values are the count for that category.
+                    /// </summary>
+                    public List<dynamic[]> TagCloud
+                    {
+                        get
+                        {
+                            var tagCloud = new List<dynamic[]>();
+                            foreach (var l in _summary.tag_cloud)
+                            {
+                                tagCloud.Add(new dynamic[]{l[0], l[1]});
+                            }
+                            return tagCloud;
+                        }
+                    }
+
+                    /// <summary>
+                    /// A dictionary where the keys are the unique categories found in the field and the values are the count for that category.
+                    /// </summary>
+                    public Dictionary<string, string[]> TermForms
+                    {
+                        get
+                        {
+                            var termForms = new Dictionary<string, string[]>();
+                            ArrayList list;
+                            foreach (var kv in _summary.term_forms)
+                            {
+                                list = new ArrayList();
+                                foreach (string term in kv.Value)
+                                {
+                                    list.Add(term);
+                                }
+                                termForms[kv.Key] = (string[]) list.ToArray("String".GetType());
+                            }
+                            return termForms;
                         }
                     }
                 }
