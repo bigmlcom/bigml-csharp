5ca23c4e22dca5160e301283e859a75dda14940a
Jose <ribes@bigml.com>
2016-06-20 15:18:41 +0200

    Manage in a right way related terms in text fields

--- BigML/Model/LocalModel.cs
+++ BigML/Model/LocalModel.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Json;
-
+using System.Text.RegularExpressions;
 
 namespace BigML
 {
@@ -9,22 +9,79 @@ namespace BigML
         public class LocalModel
         {
             readonly JsonValue _jsonObject;
-            readonly Dictionary<string, DataSet.Field> _modelFields;
+            readonly Dictionary<string, DataSet.Field> _fields;
 
-            internal LocalModel(JsonValue jsonObject, JsonValue modelFields)
+            readonly Dictionary<string, bool> _caseSensitive;
+            readonly Dictionary<string, Dictionary<string, string>> _termForms;
+
+            internal LocalModel(JsonValue jsonObject, JsonValue fields)
             {
+                Dictionary<string, bool> caseSensitive = new Dictionary<string, bool>();
+                Dictionary<string, Dictionary<string, string>> termForms = new Dictionary<string, Dictionary<string, string>>();
+
                 _jsonObject = jsonObject;
-                _modelFields = new Dictionary<string, DataSet.Field>();
-                foreach (var kv in modelFields)
+                _fields = new Dictionary<string, DataSet.Field>();
+
+                foreach (var kv in fields)
                 {
-                    _modelFields[kv.Key] = new DataSet.Field(kv.Value);
+                    string fieldId = kv.Key;
+                    _fields[fieldId] = new DataSet.Field(kv.Value);
+
+                    //text analysis initialization
+                    Dictionary<string, string> options = new Dictionary<string, string>();
+
+                    if (_fields[fieldId].OpType == OpType.Text)
+                    {
+                        DataSet.Field currentField = _fields[kv.Key];
+                        DataSet.Field.Summary.Text textSummary = (DataSet.Field.Summary.Text)currentField.FieldSummary;
+
+                        //case sensitive per field
+                        caseSensitive.Add(fieldId, (bool) currentField.TermAnalysis["case_sensitive"]);
+
+                        //term options per field
+                        string regex = "";
+                        foreach (KeyValuePair<string, string[]> termOptions in textSummary.TermForms)
+                        {
+                            regex = "(\b|_)" + termOptions.Key + "(\b|_)";
+                            int totalOptions = termOptions.Value.Length;
+                            for (int i = 0; i < totalOptions; i++)
+                            {
+                                regex += "|(\b|_)" + termOptions.Value[i] + "(\b|_)";
+                            }
+                            options.Add(termOptions.Key, regex);
+                        }
+                    }
+                    termForms[fieldId] = options;
                 }
+                _caseSensitive = caseSensitive;
+                _termForms = termForms;
             }
 
 
             private System.Globalization.NumberStyles style = System.Globalization.NumberStyles.AllowDecimalPoint;
             private System.Globalization.CultureInfo provider = new System.Globalization.CultureInfo("en-US");
 
+
+            private int termMatches(string text, string fieldLabel, string term)
+            {
+                RegexOptions flags = RegexOptions.IgnoreCase;
+
+                Dictionary<string, string> wordsMatch = _termForms[fieldLabel];
+                MatchCollection matches;
+                if (wordsMatch.ContainsKey(term))
+                {
+                    if (_caseSensitive.ContainsKey(fieldLabel))
+                    {
+                        matches = Regex.Matches(text, term);
+                    }
+                    else
+                    {
+                        matches = Regex.Matches(text, term, flags);
+                    }
+                    return matches.Count;
+                }
+                return 0;
+            }
 
 
             private int countWords(string inData, string predicateValue)
@@ -73,7 +130,8 @@ namespace BigML
                     if (children.Predicate.Term != null && children.Predicate.Term != "")
                     {
                         //is a text field
-                        inDataValue = countWords(inDataValue, children.Predicate.Term);
+                        inDataValue = termMatches(inDataValue, fieldId, children.Predicate.Term);
+                        //inDataValue = countWords(inDataValue, children.Predicate.Term);
                     }
 
                     switch (children.Predicate.Operator)
