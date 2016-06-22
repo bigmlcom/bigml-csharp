a3dc0d2e3ee2a4c89cb920986c67358b1cf2c9ee
Jose <ribes@bigml.com>
2016-06-22 17:37:32 +0200

    Add Categories enumerations

---
+++ BigML/Helpers/Categories.cs
@@ -0,0 +1,30 @@
+namespace BigML
+{
+    public enum Categories
+    {
+        Uncategorized = -1,
+        Miscellaneous = 0,
+        AutomotiveEngineeringAndManufacturing = 1,
+        EnergyOilAndGas = 2,
+        BankingAndFinance = 3,
+        FraudAndCrime = 4,
+        Healthcare = 5,
+        PhysicalEarthAndLifeSciences = 6,
+        ConsumerAndRetail = 7,
+        SportsAndGames = 8,
+        DemographicsAndSurveys = 9,
+        AerospaceAndDefense = 10,
+        ChemicalAndPharmaceutical = 11,
+        HigherEducationAndScientificResearch = 12,
+        HumanResourcesAndPsychology = 13,
+        Insurance = 14,
+        LawAndOrder = 15,
+        MediaMarketingAndAdvertising = 16,
+        PublicSectorAndNonprofit = 17,
+        ProfessionalServices = 18,
+        TechnologyAndCommunications = 19,
+        TransportationAndLogistics = 20,
+        TravelAndLeisure = 21,
+        Utilities = 22
+    }
+}
---
+++ BigML/Helpers/WhizzMLCategories.cs
@@ -0,0 +1,28 @@
+namespace BigML
+{
+    public enum WhizzMLCategories
+    {
+        Uncategorized = -1,
+        Miscellaneous = 0,
+        AdvancedWorkflow = 1,
+        AnomalyDetection = 2,
+        AssociationDiscovery = 3,
+        BasicWorkflow = 4,
+        Boosting = 5,
+        Classification = 6,
+        ClassificationOrRegression = 7,
+        Correlations = 8,
+        ClusterAnalysis = 9,
+        DataTransformation = 10,
+        Evaluation = 11,
+        FeatureEngineering = 12,
+        FeatureExtraction = 13,
+        FeatureSelection = 14,
+        HyperparameterOptimization = 15,
+        ModelSelection = 16,
+        PredictionAndScoring = 17,
+        Regression = 18,
+        Stacking = 19,
+        StatisticalTest = 20
+    }
+}
