2638804d18faec450538db5f4855be8735c55236
Jose <ribes@bigml.com>
2016-06-24 11:41:51 +0200

    Add Combiner and MissingStrategy enumarations

---
+++ BigML/Helpers/Combiner.cs
@@ -0,0 +1,10 @@
+namespace BigML
+{
+    public enum Combiner
+    {
+        Plurality = 0, 
+        ConfidenceWeighted = 1, 
+        ProbabilityWeighted = 2, 
+        ThresholdBased = 3
+    }
+}
---
+++ BigML/Helpers/MissingStrategy.cs
@@ -0,0 +1,8 @@
+namespace BigML
+{
+    public enum MissingStrategy
+    {
+        LastPrediction = 0,
+        Proportional = 1
+    }
+}
