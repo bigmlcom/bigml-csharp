64643ddc3bdd5153dd382fdeda5c378a34a6cf3a
Jose <ribes@bigml.com>
2016-09-22 20:26:13 +0200

    Initial example is sr->ds->ml->Lpred

--- Iris/Program.cs
+++ Iris/Program.cs
@@ -14,28 +14,20 @@ namespace Iris
         }
 
         /// <summary>
-        /// Simple sample that runs through all steps to explicitly create a prediction 
-        /// from a csv file with the classic iris data.
+        /// Simple sample that runs through all steps to explicitly create a
+        /// local prediction from a csv file with the classic iris data.
         /// </summary>
         static async Task MainAsync()
         {
             // New BigML client with username and API key
-            Console.Write("user: "); var User = Console.ReadLine();
-            Console.Write("key: "); var ApiKey = Console.ReadLine();
+            Console.Write("user: ");
+            var User = Console.ReadLine();
+            Console.Write("key: ");
+            var ApiKey = Console.ReadLine();
+            
 
-
-            Ordered<Source.Filterable, Source.Orderable, Source> result
-                = (from s in client.ListSources()
-                   orderby s.Created descending
-                   select s);
-
-            var sources = await result;
-
-            foreach(var src in sources)
-            {
-                Console.WriteLine(src.ToString());
-            }
-            /*
+            var client = new Client(User, ApiKey);
+            
             // New source from in-memory stream, with separate header. That's the header
             var source = await client.CreateSource(iris, "Iris.csv", "sepal length, sepal width, petal length, petal width, species");
             // No push, so we need to busy wait for the source to be processed.
@@ -50,141 +42,19 @@ namespace Iris
 
             // Default model from dataset
             var model = await client.CreateModel(dataset);
-            */
-            /*
-            Model model;
-            string modelId = "model/575085112275c16672002f5d";
             // No push, so we need to busy wait for the source to be processed.
-            while ((model = await client.Get<Model>(modelId)).StatusMessage.StatusCode != Code.Finished) await Task.Delay(10);
+            while ((model = await client.Get(model)).StatusMessage.StatusCode != Code.Finished) await Task.Delay(10);
+            Console.WriteLine(model.StatusMessage.ToString());
 
+            Console.WriteLine("Creating local model");
             Dictionary<string, dynamic> inputData = new Dictionary<string, dynamic>();
             inputData.Add("000002", 3);
             inputData.Add("000003", 1.5);
 
             var localModel = model.ModelStructure;
-            var nodoResultado = localModel.predict(inputData);
+            var nodeResult = localModel.predict(inputData);
 
-            Console.WriteLine("Predict:\n" + nodoResultado);
-            */
-
-            /*
-            //MODELO CON campo de TEXTO
-            Model model;
-            string modelId = "model/5767b5963bbd213cac004a8d";
-            while ((model = await client.Get<Model>(modelId)).StatusMessage.StatusCode != Code.Finished) await Task.Delay(10);
-            Model.LocalModel localModel = model.ModelStructure;
-            Dictionary<string, dynamic> inputData = new Dictionary<string, dynamic>();
-            inputData.Add("000000", "");
-            inputData.Add("000002", "salt in a recipe");
-            inputData.Add("00000d", 0.1);
-            Model.Node prediction = localModel.predict(inputData);
-            */
-
-            /*
-            // modelo con items
-            Model model;
-            string modelId = "model/5767b5963bbd213cac004a8d";
-            while ((model = await client.Get<Model>(modelId)).StatusMessage.StatusCode != Code.Finished) await Task.Delay(10);
-            Model.LocalModel localModel = model.ModelStructure;
-            Dictionary<string, dynamic> inputData = new Dictionary<string, dynamic>();
-            inputData.Add("000000", "");
-            inputData.Add("000002", "salt in a recipe");
-            inputData.Add("00000d", 0.1);
-            Model.Node prediction = localModel.predict(inputData);
-            */
-
-
-            
-            //Ensemble ensemble;
-            string modelId = "ensemble/5761a7fe3bbd210c6400a60a";
-            string datasetId = "dataset/5761a4d73bbd210c6b005816";
-
-            /*
-            var parameters = new BatchPrediction.Arguments();
-            // "model" parameter can be a model, an ensemble or a logisticregression
-            parameters.Add("model", modelId);
-            parameters.Add("dataset", datasetId);
-            parameters.Add("output_dataset", true);
-            BatchPrediction batchPrediction;
-            batchPrediction = await client.CreateBatchPrediction(parameters);
-            string batchPredictionId = batchPrediction.Resource;
-            // wait for finish
-            while ((batchPrediction = await client.Get<BatchPrediction>(batchPredictionId)).StatusMessage.NotSuccessOrFail()) await Task.Delay(10);
-            Console.WriteLine(batchPrediction.OutputDatasetResource);
-            */
-
-            string ensembleId = "ensemble/565ecf4d28eb3e62f6000003"; //put your Id here
-            var parameters = new Prediction.Arguments();
-            parameters.Add("ensemble", ensembleId);
-            parameters.InputData.Add("000000", 7.9);
-            parameters.InputData.Add("000001", 3.8);
-            parameters.InputData.Add("000002", 6.4);
-            parameters.InputData.Add("000003", 2);
-            Prediction ensemblePrediction = await client.CreatePrediction(parameters);
-            Console.WriteLine(ensemblePrediction.GetPredictionOutcome<string>().ToString()); //output is string for categorical models
-
-
-            // No push, so we need to busy wait for the source to be processed.
-            /*while ((ensemble = await client.Get<Ensemble>(ensembleId)).StatusMessage.StatusCode != Code.Finished) await Task.Delay(10);
-            var localEnsemble = ensemble.EnsembleStructure;
-            Model modelInEnsemble;
-            string modelId;
-            for (int i = 0; i < ensemble.Models.Count; i++)
-            {
-                modelId = ensemble.Models[i];
-                while ((modelInEnsemble = await client.Get<Model>(modelId)).StatusMessage.StatusCode != Code.Finished) await Task.Delay(10);
-                localEnsemble.addLocalModel(modelInEnsemble.ModelStructure);
-            }
-
-            Dictionary<string, dynamic> inputData = new Dictionary<string, dynamic>();
-            //inputData.Add("100000", 37);
-            inputData.Add("Age", 37);
-            inputData.Add("100001", "3rd Class");
-            inputData.Add("100002", 7060);
-            inputData.Add("100004", "servant");
-            inputData.Add("100007", "1");
-
-            var results = localEnsemble.predict(inputData);
-            
-            Console.WriteLine(results);
-            */
-
-            //Console.WriteLine("Predict:\n" + prediction);
-
-            /*
-            Console.WriteLine(model.StatusMessage.ToString());
-
-            // The model description is what we are really after
-            var description = model.ModelDescription;
-            Console.WriteLine(description.ToString());
-
-            Console.WriteLine("\n ---- Model description as a function (Output)     ------ ");
-            // First convert it to a .NET expression tree
-            var expression = description.Expression();
-            Console.WriteLine(expression.ToString());
-
-            Console.WriteLine("\n ---- Model description as a function (Confidence) ------ ");
-            var expressionConf = description.ConfidenceValue();
-            Console.WriteLine(expressionConf);
-
-            Console.WriteLine("\n ---- Model description as a function (Object with properties) ------ ");
-            var expressionCR = description.ComplexResult();
-            Console.WriteLine(expressionCR);
-
-            Console.WriteLine("\n Prediction block ------ ");
-            // Then compile the expression tree into MSIL. Only with the input fields of the model
-            var predictOutput = expression.Compile() as Func<double,double,double,double,string>;
-            var predictConfid = expressionConf.Compile() as Func<double, double, double, double, float>;
-            var predictBoth = expressionCR.Compile() as Func<double, double, double, double, Object>;
-
-            // And try the first flower of the example set.
-            var resultOutput = predictOutput(5.1, 3.5, 1.4, 0.2);
-            var resultConfid = predictConfid(5.1, 3.5, 1.4, 0.2);
-            var resultNode = predictBoth(5.1, 3.5, 1.4, 0.2);
-            Console.WriteLine("Output = {0}, expected = {1}", resultOutput, "setosa");
-            Console.WriteLine("Confidence = {0}", resultConfid);
-            Console.WriteLine("Whole node values: {0}", ((Model.ResultNode)resultNode).ToString());
-            */
+            Console.WriteLine("Predict:\n" + nodeResult);
         }
 
         private static IEnumerable<string> iris = new[]
