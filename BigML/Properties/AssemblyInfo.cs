f327de11b008b5eda88f6e80b1b413f984497c26
Jose <ribes@bigml.com>
2016-10-10 19:53:22 +0200

    Update Copyright and project platform target

--- BigML/Properties/AssemblyInfo.cs
+++ BigML/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("BigML")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyCopyright("BigML © 2013-2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
