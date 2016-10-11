5cd97a272bec1d751f61598b2b98e9eef81f71fb
Jose <ribes@bigml.com>
2016-10-11 15:02:02 +0200

    New Project in solution to use BigML with WinForms

---
+++ WFormApp/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace WFormApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.BackColor = System.Drawing.SystemColors.Info;
+            this.label2.Location = new System.Drawing.Point(13, 172);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(259, 22);
+            this.label2.TabIndex = 7;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 142);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(260, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Button without await";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.BackColor = System.Drawing.SystemColors.Info;
+            this.label1.Location = new System.Drawing.Point(13, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(259, 22);
+            this.label1.TabIndex = 5;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(260, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Button using await";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "BigML Test App";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WFormApp/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BigML;
+
+namespace WFormApp
+{
+    public partial class Form1 : Form
+    {
+        string userName = "user1";
+        string apiKey = "1a07911f835dede4607cfafa3fa4b60d9bd7XXXX";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // add async attribute in the event handler
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            label1.Text = "Loading...";
+
+            Client c = new Client(userName, apiKey);
+            Ordered<Source.Filterable, Source.Orderable, Source> result
+                = (from s in c.ListSources()
+                   orderby s.Created descending
+                   select s);
+
+            Listing<Source> sources = await result; //use await keyword
+            int countSources = 0;
+            foreach (Source src in sources)
+            {
+                countSources++;
+            }
+
+            label1.Text = countSources + " sources found";
+            button1.Enabled = true;
+        }
+
+
+        private Task<Listing<Source>> SourcesList()
+        {
+
+            Client c = new Client(userName, apiKey);
+            Ordered<Source.Filterable, Source.Orderable, Source> result
+                = (from s in c.ListSources()
+                   orderby s.Created descending
+                   select s);
+
+            return result.InternalTask;
+        }
+
+        private Task<Source> DoGetAsync()
+        {
+            Client c = new Client(userName, apiKey);
+            return c.Get<Source>("source/57e4206ba2e47604db001b20");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            label2.Text = "Loading...";
+
+            Task<Source> task1 = Task.Run(() => DoGetAsync());
+            Task<Listing<Source>> task2 = Task.Run(() => SourcesList());
+
+            Source s = task1.Result;
+            Listing<Source> s2 = task2.Result;
+
+            label2.Text = s.Name;
+            label2.Text = s2.Object["meta"]["limit"] + " sources found";
+
+            button2.Enabled = true;
+        }
+
+    }
+}
