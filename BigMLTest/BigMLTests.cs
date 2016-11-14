21fb4a469f221f25d669335eb355b0fe58c775da
Jose <ribes@bigml.com>
2016-11-14 14:29:16 +0100

    Creates two initial tests

--- BigMLTest/BigMLTests.cs
+++ BigMLTest/BigMLTests.cs
@@ -1,18 +1,51 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using BigML;
 
 namespace BigMLTest
 {
     /// <summary>
-    /// TODO: Port tests from https://github.com/bigmlcom/python/tree/master/tests/features
+    /// 
     /// </summary>
     [TestClass]
     public class BigMLTests
     {
+        string userName = "user";
+        string apiKey = "1a07911f835dede4607cfafa3aa4b60d9bdXXXXX";
+
         [TestMethod]
-        public void Vacuous()
+        public async Task CreateSourceFromRemote()
         {
-            var TODO = true;
-            Assert.AreEqual(TODO, true);
+            Client c = new Client(userName, apiKey);
+            Source.Arguments args = new Source.Arguments();
+            args.Add("remote", "https://static.bigml.com/csv/iris.csv");
+
+            Source s = await c.CreateSource(args);
+            s = await c.Wait<Source>(s.Resource);
+
+            Assert.AreEqual(s.StatusMessage.StatusCode, Code.Finished);
+
+            await c.Delete(s);
+        }
+
+        [TestMethod]
+        public async Task CreateDataset()
+        {
+            Client c = new Client(userName, apiKey);
+            Source.Arguments args = new Source.Arguments();
+            args.Add("remote", "https://static.bigml.com/csv/iris.csv");
+
+            Source s = await c.CreateSource(args);
+            s = await c.Wait<Source>(s.Resource);
+
+            Assert.AreEqual(s.StatusMessage.StatusCode, Code.Finished);
+
+            DataSet.Arguments argsDS = new DataSet.Arguments();
+            DataSet ds = await c.CreateDataset(argsDS);
+            ds = await c.Wait<DataSet>(ds.Resource);
+
+            await c.Delete(s);
+            await c.Delete(ds);
         }
     }
 }
